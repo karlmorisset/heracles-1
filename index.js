b57f1d44fec1b48f3a481a3028f06f92139e34c7
Karl Morisset <karl.morisset@wildcodeschool>
2022-03-16 17:48:20 +0100

    structure en place. Algo à affiner

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// First Labour : Heracles vs Nemean Lion
-// use your Figher class here
+const Fighter = require('./src/Fighter.js')
+
+const heracles = new Fighter("🧔 Heracles", 12, 6)
+const lion = new Fighter("🦁 Nemean Lion", 20, 13)
+
+do {
+    heracles.fight(lion)
+    lion.fight(heracles)
+} while(heracles.life > 0 && lion.life > 0)
+
+console.log(heracles);
+console.log(lion);
+
+let result = heracles.life == 0 ? "Lion a remporté le combat" : "Héraclès a remporté le combat"
+console.log(result)
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,35 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+
+class Fighter
+{
+    constructor(name, strength, dexterity) {
+        this.name = name
+        this.strength = strength
+        this.dexterity = dexterity
+        this.life = MAX_LIFE
+    }
+
+    fight(opponent) {
+        let attack = this.getRandomNumber(1, this.strength)
+        
+        let damage = opponent.dexterity - attack
+        
+        damage = this.limitToZero(damage)
+      
+        opponent.life -= damage
+        opponent.life = this.limitToZero(opponent.life)
+
+        return damage
+    }
+
+    getRandomNumber(min, max) {
+        return min + Math.floor(Math.random() * (max-min + 1))
+    }
+
+    limitToZero(property) {
+        return property < 0 ? 0 : property
+    }
+}
+
+module.exports = Fighter
